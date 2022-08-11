a35e56d73d710398a6a3f32671bd0731d6d4b26d
jaime saldana <jaisal1987@gmail.com>
2022-08-10 21:01:17 -0800

    updated js

---
+++ js/sc.js
@@ -0,0 +1,68 @@
+// Variables
+
+/*
+Camelcase (recommended js way)
+Snake_case
+Titlecase
+*/
+var firstName =
+var firstName = "Jaime";
+
+
+// Your first choice when creating l(declaring) a variabke should beb const
+const lastName = "Saldana";
+
+//Also good
+let is Happy = true;
+
+console.log (firstName);
+console.log(lastName);
+console.log(ishappy);
+
+// empty string
+
+const duckName = ""; // example of empry string
+let city; // let city = undefinded;
+
+// Variables cant be named after reserved words 
+const class = "hello";//Number
+console.log(typeof7);//Number
+console.log(typeof4.7)//Number
+console.log(typeof NaN);//Number
+console.log(typeof "");// string
+console.log(typeof "john");// string
+console.log(typeof true);//boolean
+console.log(typeof undefined);//
+
+
+// declaration and assignment
+let hobby = "soccer";
+
+// re-assignment
+hobby = 555;
+
+console.log(hobby)\
+
+// declaration and assignment (gets sealed)
+const favoritefood = "pizza;
+
+// re-assignmwnt wont work 
+favoritefood = 45454;"
+
+
+// operators 
+/// + - * / %
+
+console.log(5 * 6)
+console.log(110 + 2);
+
+// + overloading for strings
+console.log("favrotite" + "pizza")
+
+console.log("james".length);
+
+
+
+
+
+
